6af39f55b8f14f4328fa2ff50c78f7def8a78333
Igor Abdulović <igorabdul@gmail.com>
2023-10-11 13:56:49 +0200

    vjezbe 1

---
+++ vjezbe1/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int broj;
+    broj = 10;
+    int Broj = 2;
+    float decimalni1 = 1.22222;
+    double decimalni2 = 2.3383337638724786234;
+
+    cout << "Cijeli brojevi: " << broj << ", " << Broj << endl;
+    cout.precision(15);
+    cout << "Decimalni brojevi: " << decimalni1 << ", " << decimalni2 << endl;
+
+    int a = 5;
+    int b(10);
+    int c{15};
+
+    cout << "Vrijednost 'a': " << a << endl;
+    cout << "Vrijednost 'b': " << b << endl;
+    cout << "Vrijednost 'c': " << c << endl;
+
+    char karakter1 = 'l';
+
+    cout << "Karakter 1 je: " << karakter1 << endl;
+
+    bool istina = true;
+    bool laz(false);
+
+    cout << "Bool je: " << istina << ", " << laz << endl;
+
+    return 0;
+}
